cc3148620fd8bb18991abeb2222741dbafd14df8
SullyProjects <sulli544@msu.edu>
2018-12-14 20:27:45 -0500

    Finished Class-Tkinter version. Needs bug fixes.

--- FireComputer_Classes-Tkinter.py
+++ FireComputer_Classes-Tkinter.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import math as m
 
 class App(Frame):
     def __init__(self, master=None):
@@ -18,17 +18,17 @@ class App(Frame):
 
     def SS_click(self):
         self._p_mass_str = Label(self, text='Powder Mass (Kg):')
-        self._p_mass_str.grid(sticky='w',row=1,column=0)
-        self._sh_mass_str = Label(self,text='Shell Mass (Kg):')
-        self._sh_mass_str.grid(sticky='w',row=2,column=0)
+        self._p_mass_str.grid(sticky='w', row=1, column=0)
+        self._sh_mass_str = Label(self, text='Shell Mass (Kg):')
+        self._sh_mass_str.grid(sticky='w', row=2, column=0)
 
         self._p_mass = Entry(self)
-        self._p_mass.grid(sticky='w',row=1,column=1)
+        self._p_mass.grid(sticky='w', row=1, column=1)
         self._sh_mass = Entry(self)
-        self._sh_mass.grid(sticky='w',row=2,column=1)
+        self._sh_mass.grid(sticky='w', row=2, column=1)
 
-        self._calc_button = Button(self, text='Calculate',command=self.SS_calculate)
-        self._calc_button.grid(sticky='w',row=3,column=0)
+        self._calc_button = Button(self, text='Calculate', command=self.SS_calculate)
+        self._calc_button.grid(sticky='w', row=3, column=0)
 
     def SS_calculate(self):
         try:
@@ -36,17 +36,50 @@ class App(Frame):
             sh_mass = int(self._sh_mass.get())
 
         except ValueError:
-            self._error_message = Label(self,text='You can only calculate numbers.')
-            self._error_message.grid(sticky='n',row=4,column=1)
+            _error_message = Label(self, text='You can only calculate numbers.')
+            _error_message.grid(sticky='n', row=4, column=1)
 
-
-
+        vel = round((1500 * m.pow((p_mass/sh_mass), 0.45)), 2)
+        vel_txt = "Calculated Velocity: " + str(vel)
+        vel_str = Label(self, text=vel_txt)
+        vel_str.grid(sticky='w', row=5)
 
     def FOS_click(self):
-        pass
+        self._vel_str = Label(self, text='Velocity (m/s):')
+        self._vel_str.grid(sticky='w', row=1, column=0)
+        self._dist_str = Label(self, text='Target Distance (m):')
+        self._dist_str.grid(sticky='w', row=2, column=0)
+
+        self._vel = Entry(self)
+        self._vel.grid(sticky='w', row=1, column=1)
+        self._dist = Entry(self)
+        self._dist.grid(sticky='w', row=2, column=1)
+
+        self._calc_button = Button(self, text='Calculate', command=self.FOS_calculate)
+        self._calc_button.grid(sticky='w', row=3, column=0)
 
     def FOS_calculate(self):
-        pass
+        _G = 9.81
+
+        try:
+            vel = int(self._vel.get())
+            dist = int(self._dist.get())
+
+        except ValueError:
+            _error_message = Label(self, text='You can only calculate numbers.')
+            _error_message.grid(sticky='n', row=4, column=1)
+
+        angle_rad = (0.5 * m.asin((_G * dist) / (m.pow(vel, 2))))
+        angle_deg = round(m.degrees(angle_rad), 2)
+        agl_txt = "Calculated Angle (deg): "+str(angle_deg)
+        agl_str = Label(self, text=agl_txt)
+        agl_str.grid(sticky='w', row=5)
+
+        horz_vel = vel*m.cos(angle_rad)
+        flight_time = horz_vel/dist
+        flight_str = "Estimated Flight Time (S): " + str(round(flight_time,2))
+        flight_txt = Label(self, text=flight_str)
+        flight_txt.grid(sticky='w', row=6)
 
     def EXIT_click(self):
         quit()
