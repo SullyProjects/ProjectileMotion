0582ed82955149800f5afe7a6a0c8ab51703d951
SullyProjects <sulli544@msu.edu>
2018-12-18 17:19:02 -0700

    Fixed Bug where text would layer when switching between calculators

--- FireComputer_Classes-Tkinter.py
+++ FireComputer_Classes-Tkinter.py
@@ -17,6 +17,22 @@ class App(Frame):
         self._EXIT_button.grid(row=0, column=2)
 
     def SS_click(self):
+        try:
+            self._vel_str.destroy()
+            self._vel.destroy()
+            self._dist_str.destroy()
+            self._dist.destroy()
+            self._calc_button.destroy()
+
+        except AttributeError:
+            pass
+
+        try:
+            self._error_message.destroy()
+
+        except AttributeError:
+            pass
+
         self._p_mass_str = Label(self, text='Powder Mass (Kg):')
         self._p_mass_str.grid(sticky='w', row=1, column=0)
         self._sh_mass_str = Label(self, text='Shell Mass (Kg):')
@@ -36,15 +52,39 @@ class App(Frame):
             sh_mass = int(self._sh_mass.get())
 
         except ValueError:
-            _error_message = Label(self, text='You can only calculate numbers.')
-            _error_message.grid(sticky='n', row=4, column=1)
+            self._error_message = Label(self, text='You can only calculate numbers.')
+            self._error_message.grid(sticky='n', row=4, column=1)
+
+        try:
+            self._flight_txt.destroy()
+            self._agl_str.destroy()
+
+        except AttributeError:
+            pass
 
         vel = round((1500 * m.pow((p_mass/sh_mass), 0.45)), 2)
         vel_txt = "Calculated Velocity: " + str(vel)
-        vel_str = Label(self, text=vel_txt)
-        vel_str.grid(sticky='w', row=5)
+        self._vel_str = Label(self, text=vel_txt)
+        self._vel_str.grid(sticky='w', row=5)
+
 
     def FOS_click(self):
+        try:
+            self._p_mass_str.destroy()
+            self._p_mass.destroy()
+            self._sh_mass_str.destroy()
+            self._sh_mass.destroy()
+            self._calc_button.destroy()
+
+        except AttributeError:
+            pass
+
+        try:
+            self._error_message.destroy()
+
+        except AttributeError:
+            pass
+
         self._vel_str = Label(self, text='Velocity (m/s):')
         self._vel_str.grid(sticky='w', row=1, column=0)
         self._dist_str = Label(self, text='Target Distance (m):')
@@ -66,20 +106,26 @@ class App(Frame):
             dist = int(self._dist.get())
 
         except ValueError:
-            _error_message = Label(self, text='You can only calculate numbers.')
-            _error_message.grid(sticky='n', row=4, column=1)
+            self._error_message = Label(self, text='You can only calculate numbers.')
+            self._error_message.grid(sticky='n', row=4, column=1)
+
+        try:
+            self._vel_str.destroy()
+
+        except AttributeError:
+            pass
 
         angle_rad = (0.5 * m.asin((_G * dist) / (m.pow(vel, 2))))
         angle_deg = round(m.degrees(angle_rad), 2)
         agl_txt = "Calculated Angle (deg): "+str(angle_deg)
-        agl_str = Label(self, text=agl_txt)
-        agl_str.grid(sticky='w', row=5)
+        self._agl_str = Label(self, text=agl_txt)
+        self._agl_str.grid(sticky='w', row=5)
 
         horz_vel = vel*m.cos(angle_rad)
         flight_time = horz_vel/dist
         flight_str = "Estimated Flight Time (S): " + str(round(flight_time,2))
-        flight_txt = Label(self, text=flight_str)
-        flight_txt.grid(sticky='w', row=6)
+        self._flight_txt = Label(self, text=flight_str)
+        self._flight_txt.grid(sticky='w', row=6)
 
     def EXIT_click(self):
         quit()
